9a086e93f9b934af4fe75e1f590b62412aa42f22
hyojhand <sonjw5128@gmail.com>
2022-06-14 03:07:10 +0900

    [Add] JPQL

---
+++ hello-jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,49 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        // 트랜잭션 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("관리자");
+            member.setAge(10);
+            member.setType(MemberType.ADMIN);
+
+            member.setTeam(team);
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            String query = "select index(t.members) from Team t";
+            List<Integer> result = em.createQuery(query, Integer.class).getResultList ();
+
+            for (Integer s : result) {
+                System.out.println("s = " + s);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
